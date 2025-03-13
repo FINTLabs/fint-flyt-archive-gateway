ce48e599d47a2d3e1b96d8df881098d41bece168
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-03-13 12:07:30 +0100

    test with old web client configuration

--- src/main/java/no/fintlabs/flyt/WebClientConfiguration.java
+++ src/main/java/no/fintlabs/flyt/WebClientConfiguration.java
@@ -19,10 +19,10 @@ public class WebClientConfiguration {
                         ConnectionProvider
                                 .builder("laidback")
                                 .maxLifeTime(Duration.ofMinutes(30))
-                                .maxIdleTime(Duration.ofMinutes(2))
+                                .maxIdleTime(Duration.ofMinutes(5))
                                 .build())
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 120000)
-                .responseTimeout(Duration.ofSeconds(130))
+                .responseTimeout(Duration.ofSeconds(310))
         );
     }
 
