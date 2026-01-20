bd76da861c05f27201170b21e0e7d9924d4bf8e9
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-01-20 11:47:46 +0100

    temp ignore invalid messages from instance-mapped

--- src/main/java/no/novari/flyt/archive/gateway/kafka/InstanceMappedEventConsumerConfiguration.java
+++ src/main/java/no/novari/flyt/archive/gateway/kafka/InstanceMappedEventConsumerConfiguration.java
@@ -10,11 +10,23 @@ import no.novari.kafka.consuming.ErrorHandlerFactory;
 import no.novari.kafka.consuming.ListenerConfiguration;
 import no.novari.kafka.topic.name.EventTopicNameParameters;
 import no.novari.kafka.topic.name.TopicNamePrefixParameters;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.RecordDeserializationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.log.LogAccessor;
+import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.support.TopicPartitionOffset;
+import org.springframework.lang.NonNull;
 
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -60,12 +72,14 @@ public class InstanceMappedEventConsumerConfiguration {
                         .maxPollInterval(Duration.ofMinutes(30))
                         .continueFromPreviousOffsetOnAssignment()
                         .build(),
-                errorHandlerFactory.createErrorHandler(
-                        ErrorHandlerConfiguration
-                                .stepBuilder()
-                                .noRetries()
-                                .skipFailedRecords()
-                                .build()
+                new SerializationIgnoringErrorHandler(
+                        errorHandlerFactory.createErrorHandler(
+                                ErrorHandlerConfiguration
+                                        .stepBuilder()
+                                        .noRetries()
+                                        .skipFailedRecords()
+                                        .build()
+                        )
                 )
         ).createContainer(
                 EventTopicNameParameters
@@ -81,5 +95,122 @@ public class InstanceMappedEventConsumerConfiguration {
         );
     }
 
+    private static class SerializationIgnoringErrorHandler implements CommonErrorHandler {
+        private final CommonErrorHandler delegate;
+
+        private SerializationIgnoringErrorHandler(CommonErrorHandler delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public void handleOtherException(
+                @NonNull Exception exception,
+                @NonNull Consumer<?, ?> consumer,
+                @NonNull MessageListenerContainer container,
+                boolean batchListener
+        ) {
+            RecordDeserializationException recordException = findRecordDeserializationException(exception);
+            if (recordException != null) {
+                TopicPartition topicPartition = recordException.topicPartition();
+                long nextOffset = recordException.offset() + 1;
+                log.warn(
+                        "Skipping invalid record for {} at offset {}",
+                        topicPartition,
+                        recordException.offset(),
+                        exception
+                );
+                consumer.seek(topicPartition, nextOffset);
+                return;
+            }
+            delegate.handleOtherException(exception, consumer, container, batchListener);
+        }
+
+        @Override
+        public boolean handleOne(
+                @NonNull Exception exception,
+                @NonNull ConsumerRecord<?, ?> record,
+                @NonNull Consumer<?, ?> consumer,
+                @NonNull MessageListenerContainer container
+        ) {
+            return delegate.handleOne(exception, record, consumer, container);
+        }
+
+        @Override
+        public void handleRemaining(
+                @NonNull Exception exception,
+                @NonNull List<ConsumerRecord<?, ?>> records,
+                @NonNull Consumer<?, ?> consumer,
+                @NonNull MessageListenerContainer container
+        ) {
+            delegate.handleRemaining(exception, records, consumer, container);
+        }
+
+        @Override
+        public void handleBatch(
+                @NonNull Exception exception,
+                @NonNull ConsumerRecords<?, ?> records,
+                @NonNull Consumer<?, ?> consumer,
+                @NonNull MessageListenerContainer container,
+                @NonNull Runnable invokeListener
+        ) {
+            delegate.handleBatch(exception, records, consumer, container, invokeListener);
+        }
+
+        @Override
+        public boolean seeksAfterHandling() {
+            return delegate.seeksAfterHandling();
+        }
+
+        @Override
+        public boolean deliveryAttemptHeader() {
+            return delegate.deliveryAttemptHeader();
+        }
+
+        @Override
+        @NonNull
+        public LogAccessor logger() {
+            return delegate.logger();
+        }
+
+        @Override
+        public int deliveryAttempt(TopicPartitionOffset topicPartitionOffset) {
+            return delegate.deliveryAttempt(topicPartitionOffset);
+        }
+
+        @Override
+        public void clearThreadState() {
+            delegate.clearThreadState();
+        }
+
+        @Override
+        public boolean isAckAfterHandle() {
+            return delegate.isAckAfterHandle();
+        }
+
+        @Override
+        public void setAckAfterHandle(boolean ackAfterHandle) {
+            delegate.setAckAfterHandle(ackAfterHandle);
+        }
+
+        @Override
+        public void onPartitionsAssigned(
+                @NonNull Consumer<?, ?> consumer,
+                @NonNull Collection<TopicPartition> partitions,
+                @NonNull Runnable callback
+        ) {
+            delegate.onPartitionsAssigned(consumer, partitions, callback);
+        }
+
+        private static RecordDeserializationException findRecordDeserializationException(Exception exception) {
+            if (exception instanceof RecordDeserializationException recordException) {
+                return recordException;
+            }
+            Throwable cause = exception.getCause();
+            if (cause instanceof RecordDeserializationException recordException) {
+                return recordException;
+            }
+            return null;
+        }
+    }
 
 }
