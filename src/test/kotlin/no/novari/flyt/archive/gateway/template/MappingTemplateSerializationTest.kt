301c07f1735c44e3bc9e5b8a54e13d763782234b
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-05-15 12:28:38 +0200

    fiks Jackson serialisering av template

---
+++ src/test/kotlin/no/novari/flyt/archive/gateway/template/MappingTemplateSerializationTest.kt
@@ -0,0 +1,53 @@
+package no.novari.flyt.archive.gateway.template
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.assertj.core.api.Assertions.assertThatCode
+import org.junit.jupiter.api.Test
+
+class MappingTemplateSerializationTest {
+    private val objectMapper = jacksonObjectMapper()
+
+    private val archiveTemplateService =
+        ArchiveTemplateService(
+            SearchParametersTemplateService(),
+            SakTemplateService(
+                KlasseringTemplateService(SkjermingTemplateService()),
+                SkjermingTemplateService(),
+                JournalpostTemplateService(
+                    KorrespondansepartTemplateService(
+                        AdresseTemplateService(),
+                        KontaktinformasjonTemplateService(),
+                        SkjermingTemplateService(),
+                    ),
+                    DokumentbeskrivelseTemplateService(
+                        DokumentobjektTemplateService(),
+                        SkjermingTemplateService(),
+                    ),
+                    SkjermingTemplateService(),
+                ),
+                PartTemplateService(
+                    AdresseTemplateService(),
+                    KontaktinformasjonTemplateService(),
+                ),
+            ),
+            JournalpostTemplateService(
+                KorrespondansepartTemplateService(
+                    AdresseTemplateService(),
+                    KontaktinformasjonTemplateService(),
+                    SkjermingTemplateService(),
+                ),
+                DokumentbeskrivelseTemplateService(
+                    DokumentobjektTemplateService(),
+                    SkjermingTemplateService(),
+                ),
+                SkjermingTemplateService(),
+            ),
+        )
+
+    @Test
+    fun createTemplateShouldBeSerializable() {
+        val template = archiveTemplateService.createTemplate()
+
+        assertThatCode { objectMapper.writeValueAsString(template) }.doesNotThrowAnyException()
+    }
+}
