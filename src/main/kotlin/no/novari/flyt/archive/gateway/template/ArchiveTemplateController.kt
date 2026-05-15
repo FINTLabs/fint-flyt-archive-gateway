7f8781260f98ed6c5ec6c2bb20172527ceebca6c
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-05-15 10:58:19 +0200

    legg til endepunkt for arkiv template

---
+++ src/main/kotlin/no/novari/flyt/archive/gateway/template/ArchiveTemplateController.kt
@@ -0,0 +1,17 @@
+package no.novari.flyt.archive.gateway.template
+
+import no.novari.flyt.archive.gateway.template.model.MappingTemplate
+import no.novari.flyt.webresourceserver.UrlPaths.INTERNAL_API
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("$INTERNAL_API/arkiv/template")
+class ArchiveTemplateController(
+    private val archiveTemplateService: ArchiveTemplateService,
+) {
+    @GetMapping
+    fun getTemplate(): ResponseEntity<MappingTemplate> = ResponseEntity.ok(archiveTemplateService.createTemplate())
+}
