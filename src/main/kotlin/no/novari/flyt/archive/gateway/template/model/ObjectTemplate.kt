d3494bc9db97c3abb16769cbd6913034ee8b0839
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-05-15 12:33:14 +0200

    fiks Jackson serialisering av template

--- src/main/kotlin/no/novari/flyt/archive/gateway/template/model/ObjectTemplate.kt
+++ src/main/kotlin/no/novari/flyt/archive/gateway/template/model/ObjectTemplate.kt
@@ -11,25 +11,25 @@ import no.novari.flyt.archive.gateway.template.validation.UniqueOrders
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class ObjectTemplate(
     val valueTemplates: Collection<
-            @NotNull @Valid
-            ElementTemplate<ValueTemplate>,
-        > = emptyList(),
+        @NotNull @Valid
+        ElementTemplate<ValueTemplate>,
+    > = emptyList(),
     val selectableValueTemplates: Collection<
-            @NotNull @Valid
-            ElementTemplate<SelectableValueTemplate>,
-        > = emptyList(),
+        @NotNull @Valid
+        ElementTemplate<SelectableValueTemplate>,
+    > = emptyList(),
     val valueCollectionTemplates: Collection<
-            @NotNull @Valid
-            ElementTemplate<CollectionTemplate<ValueTemplate>>,
-        > = emptyList(),
+        @NotNull @Valid
+        ElementTemplate<CollectionTemplate<ValueTemplate>>,
+    > = emptyList(),
     val objectTemplates: Collection<
-            @NotNull @Valid
-            ElementTemplate<ObjectTemplate>,
-        > = emptyList(),
+        @NotNull @Valid
+        ElementTemplate<ObjectTemplate>,
+    > = emptyList(),
     val objectCollectionTemplates: Collection<
-            @NotNull @Valid
-            ElementTemplate<CollectionTemplate<ObjectTemplate>>,
-        > = emptyList(),
+        @NotNull @Valid
+        ElementTemplate<CollectionTemplate<ObjectTemplate>>,
+    > = emptyList(),
 ) {
     companion object {
         @JvmStatic
