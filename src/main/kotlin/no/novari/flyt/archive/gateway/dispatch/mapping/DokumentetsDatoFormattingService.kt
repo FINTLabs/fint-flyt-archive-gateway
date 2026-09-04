2505e15aaac83b1cdfab17e84e6bd525c2c9ad28
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-09-04 14:56:39 +0200

    godta dato i format YYYY-MM-DD

--- src/main/kotlin/no/novari/flyt/archive/gateway/dispatch/mapping/DokumentetsDatoFormattingService.kt
+++ src/main/kotlin/no/novari/flyt/archive/gateway/dispatch/mapping/DokumentetsDatoFormattingService.kt
@@ -1,6 +1,7 @@
 package no.novari.flyt.archive.gateway.dispatch.mapping
 
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
@@ -14,17 +15,30 @@ class DokumentetsDatoFormattingService {
     }
 
     fun validateAndFormat(dokumentetsDato: String): String {
-        val parsedDokumentetsDato =
-            runCatching { LocalDateTime.parse(dokumentetsDato, FORMATTER) }
-                .getOrElse { throw InvalidDokumentetsDatoException(dokumentetsDato) }
+        val parsedDateTime =
+            runCatching {
+                LocalDateTime.parse(dokumentetsDato, DATE_TIME_FORMATTER)
+            }.getOrElse {
+                runCatching {
+                    LocalDate.parse(dokumentetsDato, DATE_FORMATTER).atStartOfDay()
+                }.getOrElse {
+                    throw InvalidDokumentetsDatoException(dokumentetsDato)
+                }
+            }
 
-        return FORMATTER.format(parsedDokumentetsDato)
+        return DATE_TIME_FORMATTER.format(parsedDateTime)
     }
 
     companion object {
-        private val FORMATTER: DateTimeFormatter =
+        private val DATE_TIME_FORMATTER: DateTimeFormatter =
             DateTimeFormatterBuilder()
                 .appendPattern("uuuu-MM-dd'T'HH:mm:ss'Z'")
+                .toFormatter()
+                .withResolverStyle(ResolverStyle.STRICT)
+
+        private val DATE_FORMATTER: DateTimeFormatter =
+            DateTimeFormatterBuilder()
+                .appendPattern("uuuu-MM-dd")
                 .toFormatter()
                 .withResolverStyle(ResolverStyle.STRICT)
     }
